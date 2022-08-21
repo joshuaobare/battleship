2f349c5a353d69cbc8c81f18438b7d485155ca80
joshuaobare <jobare.obare@gmail.com>
2022-08-21 16:27:54 +0300

    actuals populated. Ship object to be restructured

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -9,6 +9,11 @@ const Ship = require("./ship")
 
 const playerBoard = new Gameboard()
 const computerBoard = new Gameboard()
+const patrol = new Ship(2,[0,0])
+const submarine = new Ship(3,[1,1])
+const destroyer = new Ship(3,[1,2])
+const battleship = new Ship(4,[9,0])
+const carrier = new Ship(5,[8,0])
 
 const player = new Player("Player 1",computerBoard)
 const computer = new Player("computer",playerBoard)
